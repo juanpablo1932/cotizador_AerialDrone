1e80f49d924b7577aca5b7708e944e1ae7217f23
Juan <juandiazecheverry@gmail.com>
2022-01-22 01:45:51 -0500

    Validar numeros negativos

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,131 +46,138 @@ class ValorFotos {
 $(`#form1`).on("submit", function () {
   const tipoDrone = $(`#tipoUas`).val();
   const horasVuelo = $(`#horasVuelo`).val();
+  let validadorNegativo = Math.sign(horasVuelo);
+  if (validadorNegativo === 1) {
+    const precio1 = new ValorHora(tipoDrone, horasVuelo);
+    let drone = precio1.seleccionDrone();
+    console.log(drone);
+    let horas = precio1.seleccionHoras();
+    console.log(horas);
+    console.log(precio1);
 
-  const precio1 = new ValorHora(tipoDrone, horasVuelo);
-  let drone = precio1.seleccionDrone();
-  console.log(drone);
-  let horas = precio1.seleccionHoras();
-  console.log(horas);
-  console.log(precio1);
-
-  function precioDrone() {
-    if (drone === "Ala fija") {
-      return 150000 * horas;
-    } else if (drone === "Multirotor") {
-      return 100000 * horas;
+    function precioDrone() {
+      if (drone === "Ala fija") {
+        return 150000 * horas;
+      } else if (drone === "Multirotor") {
+        return 100000 * horas;
+      }
     }
-  }
 
-  let precio = precioDrone();
-  console.log(precio);
+    let precio = precioDrone();
+    console.log(precio);
 
-  mostrarCotizacion(precio1);
+    mostrarCotizacion(precio1);
 
-  // eliminar elementos
-  function mostrarCotizacion(coti) {
-    let formulario = document.getElementById("containerGeneral");
-    formulario.parentNode.removeChild(formulario);
-    //agregar elementos
-    $("#main").prepend(
-      `<div class="info-coti"><p>Hola, tu cotización tiene un costo de <span id="spanPrecio">${precio} COP</span>. Consta de una operación con UAS de tipo <span id="spanDrone">${drone}</span> con una duración de <span id="spanHora">${horasVuelo} horas</span>. <br> Para realizar otra cotización oprima el siguiente botón <input type="button" value="Cotizador" onclick="location.reload()"/></p></div> `
-    );
-    $("#spanPrecio")
-      .fadeOut("fast")
-      .fadeIn(2000, function () {
-        $("#spanDrone")
-          .fadeOut("fast")
-          .fadeIn(2000, function () {
-            $("#spanHora").fadeOut("fast").fadeIn(2000);
-          });
-      });
+    // eliminar elementos
+    function mostrarCotizacion(coti) {
+      let formulario = document.getElementById("containerGeneral");
+      formulario.parentNode.removeChild(formulario);
+      //agregar elementos
+      $("#main").prepend(
+        `<div class="info-coti"><p>Hola, tu cotización tiene un costo de <span id="spanPrecio">${precio} COP</span>. Consta de una operación con UAS de tipo <span id="spanDrone">${drone}</span> con una duración de <span id="spanHora">${horasVuelo} horas</span>. <br> Para realizar otra cotización oprima el siguiente botón <input type="button" value="Cotizador" onclick="location.reload()"/></p></div> `
+      );
+      $("#spanPrecio")
+        .fadeOut("fast")
+        .fadeIn(2000, function () {
+          $("#spanDrone")
+            .fadeOut("fast")
+            .fadeIn(2000, function () {
+              $("#spanHora").fadeOut("fast").fadeIn(2000);
+            });
+        });
+    }
+  } else {
+    alert("Digita solo numeros positivos y mayores que 0");
   }
 });
 
 // COMPRA DE FOTOS ALEATORIAS
-
 function comprarFotos() {
   listaFotos = [];
 
   const calidad = $(`#calidad`).val();
   const cantidad = $(`#cantidad`).val();
-
-  let articuloIngresado = new ValorFotos(calidad, cantidad);
-  console.log(articuloIngresado);
-  let precio = articuloIngresado.calculoPrecio();
-  console.log(precio);
-  articuloIngresado = listaFotos.push(
-    new ValorFotos(calidad, cantidad, precio)
-  );
-  console.log(listaFotos);
-
-  listaFotos.forEach((foto) => {
-    let fotoStringifada = JSON.stringify(foto);
-    localStorage.setItem(foto.id, fotoStringifada);
-  });
-  const tomarTodoLocal = () => {
-    for (let i = 0; i < localStorage.length; i++) {
-      let clave = localStorage.key(i);
-      console.log("Clave: " + clave);
-      console.log("Valor: " + localStorage.getItem(clave));
-    }
-  };
-  tomarTodoLocal();
-  mostrarPrecio(articuloIngresado);
-
-  // eliminar elementos
-  function mostrarPrecio(valor) {
-    let formulario = document.getElementById("containerGeneral");
-    formulario.parentNode.removeChild(formulario);
-    //agregar elementos
-    $("#main").prepend(
-      `<div class="info-foto"><p>Hola, el costo de tus <span id="spanCantidad">${cantidad}</span> fotografías en calidad de <span id="spanCalidad">${calidad} MP</span>, tienen un costo de <span id="spanCosto">${
-        precio * cantidad
-      } COP</span> <br> Para realizar otra compra oprime el siguiente botón <input type="button" value="Comprar" onclick="location.reload()"/> <br> <button id="btnFicha">Ver ficha técnica</button> <div id="mostrarFicha"></div> </p></div> `
+  let validadorNegativo = Math.sign(cantidad);
+  if (validadorNegativo === 1) {
+    let articuloIngresado = new ValorFotos(calidad, cantidad);
+    console.log(articuloIngresado);
+    let precio = articuloIngresado.calculoPrecio();
+    console.log(precio);
+    articuloIngresado = listaFotos.push(
+      new ValorFotos(calidad, cantidad, precio)
     );
-    $("#spanCantidad")
-      .fadeOut("fast")
-      .fadeIn(2000, function () {
-        $("#spanCalidad")
-          .fadeOut("fast")
-          .fadeIn(2000, function () {
-            $("#spanCosto").fadeOut("fast").fadeIn(2000);
-          });
-      });
-  }
+    console.log(listaFotos);
 
-  // INICIO Llamada AJAX
-  const UrlDelJson = "uas.json";
-  // let para limitar la impresión de la ficha técnica
-  let numAppend = 0;
-  $("#btnFicha").click(() => {
-    $.getJSON(UrlDelJson, (respuesta, status) => {
-      if (status === "success") {
-        let misDatos = respuesta;
-        if (numAppend == 0) {
-          if (calidad === "12") {
-            $("#mostrarFicha").append(
-              `<li>Drone modelo: ${misDatos["uas"][0]["model"]}</li><li>Calidad de imagen de ${misDatos["uas"][0]["calidad"]} MP</li><li>Apertura de ${misDatos["uas"][0]["apertura"]}</li><li>CMOS de ${misDatos["uas"][0]["cmos"]} pulgadas</li>`
-            );
-            numAppend++;
-          } else if (calidad === "20") {
-            $("#mostrarFicha").append(
-              `<li>Drone modelo: ${misDatos["uas"][1]["model"]}</li><li>Calidad de imagen de ${misDatos["uas"][1]["calidad"]} MP</li><li>Apertura de ${misDatos["uas"][1]["apertura"]}</li><li>CMOS de ${misDatos["uas"][1]["cmos"]} pulgadas</li>`
-            );
-            numAppend++;
-          } else if (calidad === "48") {
-            $("#mostrarFicha").append(
-              `<li>Drone modelo: ${misDatos["uas"][2]["model"]}</li><li>Calidad de imagen de ${misDatos["uas"][2]["calidad"]} MP</li><li>Apertura de ${misDatos["uas"][2]["apertura"]}</li><li>CMOS de ${misDatos["uas"][2]["cmos"]} pulgadas</li>`
-            );
-            numAppend++;
-          }
-        } else {
-          console.log("Solo se imprime una vez la ficha técnica");
-        }
-      }
+    listaFotos.forEach((foto) => {
+      let fotoStringifada = JSON.stringify(foto);
+      localStorage.setItem(foto.id, fotoStringifada);
     });
-  });
-  // FIN Llamada AJAX
+    const tomarTodoLocal = () => {
+      for (let i = 0; i < localStorage.length; i++) {
+        let clave = localStorage.key(i);
+        console.log("Clave: " + clave);
+        console.log("Valor: " + localStorage.getItem(clave));
+      }
+    };
+    tomarTodoLocal();
+    mostrarPrecio(articuloIngresado);
+
+    // eliminar elementos
+    function mostrarPrecio(valor) {
+      let formulario = document.getElementById("containerGeneral");
+      formulario.parentNode.removeChild(formulario);
+      //agregar elementos
+      $("#main").prepend(
+        `<div class="info-foto"><p>Hola, el costo de tus <span id="spanCantidad">${cantidad}</span> fotografías en calidad de <span id="spanCalidad">${calidad} MP</span>, tienen un costo de <span id="spanCosto">${
+          precio * cantidad
+        } COP</span> <br> Para realizar otra compra oprime el siguiente botón <input type="button" value="Comprar" onclick="location.reload()"/> <br> <button id="btnFicha">Ver ficha técnica</button> <div id="mostrarFicha"></div> </p></div> `
+      );
+      $("#spanCantidad")
+        .fadeOut("fast")
+        .fadeIn(2000, function () {
+          $("#spanCalidad")
+            .fadeOut("fast")
+            .fadeIn(2000, function () {
+              $("#spanCosto").fadeOut("fast").fadeIn(2000);
+            });
+        });
+    }
+
+    // INICIO Llamada AJAX
+    const UrlDelJson = "uas.json";
+    // let para limitar la impresión de la ficha técnica
+    let numAppend = 0;
+    $("#btnFicha").click(() => {
+      $.getJSON(UrlDelJson, (respuesta, status) => {
+        if (status === "success") {
+          let misDatos = respuesta;
+          if (numAppend == 0) {
+            if (calidad === "12") {
+              $("#mostrarFicha").append(
+                `<li>Drone modelo: ${misDatos["uas"][0]["model"]}</li><li>Calidad de imagen de ${misDatos["uas"][0]["calidad"]} MP</li><li>Apertura de ${misDatos["uas"][0]["apertura"]}</li><li>CMOS de ${misDatos["uas"][0]["cmos"]} pulgadas</li>`
+              );
+              numAppend++;
+            } else if (calidad === "20") {
+              $("#mostrarFicha").append(
+                `<li>Drone modelo: ${misDatos["uas"][1]["model"]}</li><li>Calidad de imagen de ${misDatos["uas"][1]["calidad"]} MP</li><li>Apertura de ${misDatos["uas"][1]["apertura"]}</li><li>CMOS de ${misDatos["uas"][1]["cmos"]} pulgadas</li>`
+              );
+              numAppend++;
+            } else if (calidad === "48") {
+              $("#mostrarFicha").append(
+                `<li>Drone modelo: ${misDatos["uas"][2]["model"]}</li><li>Calidad de imagen de ${misDatos["uas"][2]["calidad"]} MP</li><li>Apertura de ${misDatos["uas"][2]["apertura"]}</li><li>CMOS de ${misDatos["uas"][2]["cmos"]} pulgadas</li>`
+              );
+              numAppend++;
+            }
+          } else {
+            console.log("Solo se imprime una vez la ficha técnica");
+          }
+        }
+      });
+    });
+    // FIN Llamada AJAX
+  } else {
+    alert("Digita solo numeros positivos y mayores que 0");
+  }
 }
 
 // VALIDAR COMPRA DE FOTOS
@@ -178,5 +185,7 @@ $(`#form2`).on("submit", function () {
   const calidad = $(`#calidad`).val();
   if (calidad != 12 && calidad != 20 && calidad != 48) {
     alert("Hubo un error. Digita solo el numero 12, 20 ó 48");
-  } else [comprarFotos()];
+  } else {
+    comprarFotos();
+  }
 });
